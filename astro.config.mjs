0b408ce3e1a9d11f3cdec480175ebacae59d4c26
KadhirTheEngineer <kadhir.vivek@gmail.com>
2025-11-09 20:51:38 -0600

    more fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+const repoBase = '/kadhirV-portfolio/';
+const isGitHubActions = process.env.GITHUB_ACTIONS === 'true';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	site: 'https://kadhirtheengineer.github.io/kadhirV-portfolio',
+	base: isGitHubActions ? repoBase : '/',
+});

3e9f7107335a2c8d03c6f3da32844c5e085c6b73
KadhirTheEngineer <kadhir.vivek@gmail.com>
2025-11-09 21:40:54 -0600

    i did not fix shit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-const repoBase = '/kadhirV-portfolio/';
-const isGitHubActions = process.env.GITHUB_ACTIONS === 'true';
-
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://kadhirtheengineer.github.io/kadhirV-portfolio',
-	base: isGitHubActions ? repoBase : '/',
+	site: 'https://kadhir.me',
+	base: '/',
 });
